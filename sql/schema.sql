7eb4fb7d917113652e4372a9decf5c1646339d46
chirag259 <114747682+chirag259@users.noreply.github.com>
2025-12-27 20:14:34 +0530

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,17 +1,3 @@
 CREATE DATABASE IF NOT EXISTS pharma_dw;
 USE pharma_dw;
 
-CREATE TABLE IF NOT EXISTS sponsor (
-    sponsor_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) UNIQUE
-);
-
-CREATE TABLE IF NOT EXISTS study (
-    study_id VARCHAR(50) PRIMARY KEY,
-    title TEXT,
-    status VARCHAR(50),
-    phase VARCHAR(50),
-    start_date DATE,
-    sponsor_id INT,
-    FOREIGN KEY (sponsor_id) REFERENCES sponsor(sponsor_id)
-);
